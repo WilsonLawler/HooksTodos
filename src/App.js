85731d7243d484277b1acf9458fbdeee4f4a4d9c
wilsonH <wilsonh@wilsonHdeMacBook-Pro.local>
2020-07-12 13:30:48 +0800

    combine context and useReducer to setup state management

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react';
+
+
+const initState = {
+  todos: [
+    {
+      id: 1,
+      text: 'buy a computer'
+    },
+    {
+      id: 2,
+      text: 'install vscode'
+    },
+    {
+      id: 3,
+      text: 'read bible'
+    }
+  ]
+}
+
+const reducer = (state, action) => {
+  const { type } = action;
+  switch (type) {
+    default:
+      return initState
+  }
+}
+
+export const TodosContext = React.createContext();
+
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodosContext.Provider value={{state, dispatch}}>
+    </TodosContext.Provider>
   );
 }
 
